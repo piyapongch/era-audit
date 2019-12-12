2e2b26a9b15dd2a3370a934f2b46fe6c3cf6f597
piyapongch <charoenw@ualberta.ca>
2019-12-12 15:28:32 -0700

    Add pid and handle fields

--- object-info/xmlquery_foxml.go
+++ object-info/xmlquery_foxml.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/antchfx/xmlquery"
 )
 
 func main() {
+
+	// create output file
 	file, err := os.Create("result.csv")
 	checkError("Cannot create file", err)
 	defer file.Close()
@@ -17,29 +20,37 @@ func main() {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	f, err1 := os.Open("../sample_data/wolves_foxml.xml")
+	// open foxml
+	f, err1 := os.Open("../sample_data/foxml_wolves.xml")
 	doc, err1 := xmlquery.Parse(f)
 	if err1 != nil {
-		panic(err)
+		panic(err1)
 	}
 
-	var cb []string
-	channel := xmlquery.FindOne(doc, "//foxml:digitalObject")
-	n := channel.SelectAttr("PID")
-	fmt.Printf("PID: %s\n", n)
-	cb = append(cb, n)
+	// csv string array
+	var csv []string
+	var val string
 
-	channel = xmlquery.FindOne(doc, "/foxml:digitalObject/foxml:datastream[2]/foxml:datastreamVersion[last()]/foxml:xmlContent/dc/dc:subject[last()]")
-	n = channel.InnerText()
-	fmt.Printf("subject: %s\n", n)
-	cb = append(cb, n)
-	n = ""
-	cb = append(cb, n)
+	// PID
+	n := xmlquery.FindOne(doc, "/foxml:digitalObject")
+	if n != nil {
+		val = n.SelectAttr("PID")
+	}
+	fmt.Printf("PID: %s\n", val)
+	csv = append(csv, val)
 
-	n = ""
-	cb = append(cb, n)
+	// handle
+	for i, n := range xmlquery.Find(doc, "/foxml:digitalObject/foxml:datastream[@ID='DCQ']/foxml:datastreamVersion[last()]/foxml:xmlContent/dc/dcterms:identifier") {
+		if val = ""; n != nil {
+			if hd := n.InnerText(); strings.HasPrefix(hd, "http://hdl.handle.net") {
+				val = hd
+			}
+		}
+		fmt.Printf("handle: #%d %s\n", i, val)
+		csv = append(csv, val)
+	}
 
-	err = writer.Write(cb)
+	err = writer.Write(csv)
 	checkError("Cannot write to file", err)
 }
 
